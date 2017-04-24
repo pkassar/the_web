810c8f6094d8260bfb08902c8bd8f8264fd5a39d
Filipe Fasolin <ffasolin@gmail.com>
2017-04-24 16:53:22 +0100

    Uses params hash to pass cat's name and age

--- app.rb
+++ app.rb
@@ -10,12 +10,21 @@ get '/secret' do
 end
 
 
-get '/cat' do
-  @cat_name = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
 
 
 get '/cat2' do
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params[:name]
+  p params[:age]
+  p params
+  @name = params[:name]
+  @age = params[:age]
   erb(:index)
 end
